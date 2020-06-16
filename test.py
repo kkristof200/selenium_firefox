415053cf1bea933aae38370c97a110810d1c8e6d
Kovács Kristóf-Attila <kovacskristof200@gmail.com>
2020-06-16 20:59:40 +0300

    new version

--- test.py
+++
@@ -1,6 +0,0 @@
-from selenium_firefox import Firefox
-
-res = Firefox.generate_xpath('span', id_='custom_id', class_='custom-class', attributes={'key':'value'}, for_sub_element=True)
-print(res)
-
-f = Firefox('', '')
